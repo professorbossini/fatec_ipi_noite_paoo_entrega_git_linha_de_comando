a02c547398686d1865577cd4dc6d7d70848a035a
Rodrigo Bossini <professorbossini@gmail.com>
2020-05-16 14:59:45 -0300

    Agora estamos lendo o nome do usuario

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,10 @@
+import javax.swing.JOptionPane;
+
+//Estou aprendendo a mexer com o Git na linha de comando
 public class EntregaGit {
     public static void main(String... args) {
         System.out.println("Entregando tarefas com o Git na linha de comando...");
+        String nome = JOptionPane.showInputDialog("Qual seu nome?");
+        JOptionPane.showMessageDialog(null, String.format("Oi, %s", nome));
     }
 }
